dbdf3fe2131b34a4aad18fab98c594c4c4ff0f52
Lucca <collas.lucca@outlook.fr>
2025-09-07 13:20:21 +0200

    opti

--- vite.config.js
+++ vite.config.js
@@ -205,6 +205,7 @@ logger.error = (msg, options) => {
 }
 
 export default defineConfig({
+	base: '/portfolio/',
 	customLogger: logger,
 	plugins: [
 		...(isDev ? [inlineEditPlugin(), editModeDevPlugin()] : []),
